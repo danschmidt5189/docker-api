3517c06cbd0ac2e7741dc21bc46d3133e6bfdb08
Daniel Schmidt <dcschmidt@berkeley.edu>
2025-04-28 08:31:26 -0700

    Start Swarm for the Docker tests

--- script/install_docker.sh
+++ script/install_docker.sh
@@ -1,32 +1,30 @@
-#!/bin/bash
-set -ex
+#!/bin/bash -ex
 
-declare -a SEMVER
+${DOCKER_VERSION:?You must set the 'DOCKER_VERSION' environment variable}
 
-# argv[0]
-DOCKER_VERSION=$1
-# argv[1]
-DOCKER_CE=$2
+apt-get remove -y \
+  docker \
+  docker-engine \
+  docker.io \
+  containerd \
+  runc
 
-# disable travis default installation
-systemctl stop docker.service
-apt-get -y --purge remove docker docker-engine docker.io containerd runc
+apt-get update -y
 
-# install gpg key for docker rpo
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
+apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common
 
-# enable docker repo
-add-apt-repository \
-  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) \
-  stable"
-apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update -y
 apt-cache gencaches
-
-set +e
-# install package
-apt-get install docker-ce=${DOCKER_VERSION}
+apt-get install -y \
+  docker-ce=$( apt-cache madison docker-ce | grep -e $DOCKER_VERSION | cut -f 2 -d '|' | head -1 | sed 's/\s//g' )
 
 if [ $? -ne 0 ]; then
   echo "Error: Could not install ${DOCKER_VERSION}"
@@ -34,11 +32,7 @@ if [ $? -ne 0 ]; then
   apt-cache madison docker-ce
   exit 1
 fi
-set -e
 
-systemctl stop docker.service
+systemctl start docker
 
-echo 'DOCKER_OPTS="-H unix:///var/run/docker.sock --pidfile=/var/run/docker.pid"' > /etc/default/docker
-cat /etc/default/docker
-
-systemctl start docker.service
+docker swarm init
